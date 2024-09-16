6363384b42090285d5cf6c763547caff4821dc57
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2024-09-16 19:25:04 +0200

    Add pipe spawning

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var pipe: PackedScene;
+@export var max_pipes_on_screen := 2;
 
 var score: int = 0;
 
@@ -9,24 +10,30 @@ func _ready() -> void:
 	self.start_game();
 	
 func start_game():
-	$ScoreTimer.start();
+	$PipeSpawnTimer.start();
 	
 func game_over():
 	print_debug("Game over");
-	$ScoreTimer.stop();
+	$PipeSpawnTimer.stop();
 	
 	# Wait a few seconds before stopping the ground
 	await get_tree().create_timer(1.5).timeout;
 	$Ground.moving = false;
+	self.get_tree().call_group("pipes", "stop_moving");
 	
 	$Hud.show_restart_ui();
+	
+func spawn_pipe():
+	var pipeScene := self.pipe.instantiate();
+	self.add_child(pipeScene);
 	
 func on_bird_died():
 	game_over();
 
-func _on_score_timer_timeout() -> void:
-	self.score += 1;
-	$Hud.update_score(self.score);
+func _on_pipe_spawn_timer_timeout() -> void:
+	var pipeCount = get_tree().get_nodes_in_group("pipes").size();
+	if pipeCount < self.max_pipes_on_screen:
+		self.spawn_pipe();
 
 func _on_hud_restart_clicked() -> void:
 	get_tree().reload_current_scene();
--- scripts/pipe.gd
+++ scripts/pipe.gd
@@ -1,2 +1,46 @@
 extends Node2D
-class_name Pipe;
+
+@export var speed := 100.0;
+@export var pipe_hole_height := 160;
+@export var pipe_min_height := 64;
+@export var offscreen_spawn_offset := 32;
+@export var ground_size := 64;
+var screen_size: Vector2;
+var pipe_size: Vector2;
+var moving = true;
+
+func _ready() -> void:
+	self.screen_size = get_viewport_rect().size;
+	self.pipe_size = $Up/CollisionShape2D.shape.get_rect().size;
+	self.setup_pipe();
+	
+func _process(delta: float) -> void:
+	if self.moving:
+		move_pipe(delta);
+	destroy_on_screen_exit();
+	
+func move_pipe(delta: float):
+	self.position.x -= delta * self.speed;
+	
+func stop_moving():
+	self.moving = false;
+	
+func destroy_on_screen_exit():
+	var pipe_right_edge = self.position.x + self.pipe_size.x / 2;
+	if pipe_right_edge <= 0:
+		self.queue_free();
+		
+func setup_pipe():
+	# starting position is offscreen
+	self.position.x = screen_size.x + pipe_size.x / 2 + offscreen_spawn_offset;
+	
+	var up_pipe := $Up;
+	var down_pipe := $Down;
+	var max_top_height := pipe_size.y / 2;
+	
+	var top_pipe_height := randi_range(self.pipe_min_height, max_top_height);
+	var top_pipe_bottom := top_pipe_height + pipe_size.y / 2;
+	var bottom_pipe_height := top_pipe_bottom + self.pipe_hole_height + pipe_size.y / 2;
+	
+	up_pipe.position.y = bottom_pipe_height;
+	down_pipe.position.y = top_pipe_height;
